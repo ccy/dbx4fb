d6454be5dd80547758ed8016347717b4659e783c
ccy <ccy>
2007-06-14 07:07:54 +0000

    no message

---
+++ project/fb4.dpr
@@ -0,0 +1,44 @@
+(* Build Instruction:
+
+     Output Directory: $(OutputDir)\$(ActiveProjectModule)
+Unit Output Directory: $(UnitOutputDir)\$(ActiveProjectModule)
+     Host Application: $(ActiveHostApplication)
+*)
+
+library fb4;
+
+{ Important note about DLL memory management: ShareMem must be the
+  first unit in your library's USES clause AND your project's (select
+  Project-View Source) USES clause if your DLL exports any procedures or
+  functions that pass strings as parameters or function results. This
+  applies to all strings passed to and from your DLL--even those that
+  are nested in records and classes. ShareMem is the interface unit to
+  the BORLNDMM.DLL shared memory manager, which must be deployed along
+  with your DLL. To avoid using BORLNDMM.DLL, pass string information
+  using PChar or ShortString parameters. }
+
+{$R 'fb4.res' 'fb4.rc'}
+
+uses
+  SysUtils,
+  Windows,
+  IB_Header in '..\..\core\source\rtl\IB_Header.pas',
+  firebird.client in '..\..\core\source\rtl\firebird.client.pas',
+  firebird.client.debug in '..\..\core\source\rtl\firebird.client.debug.pas',
+  firebird.dsql in '..\..\core\source\rtl\firebird.dsql.pas',
+  dbx4.base in '..\source\dbx4.base.pas',
+  dbx4.firebird.dll in '..\source\dbx4.firebird.dll.pas',
+  dbx4.firebird.driver in '..\source\dbx4.firebird.driver.pas',
+  dbx4.firebird.connection in '..\source\dbx4.firebird.connection.pas',
+  dbx4.firebird.command in '..\source\dbx4.firebird.command.pas',
+  dbx4.firebird.reader in '..\source\dbx4.firebird.reader.pas',
+  dbx4.firebird.row in '..\source\dbx4.firebird.row.pas',
+  dbx4.firebird.metadata in '..\source\dbx4.firebird.metadata.pas',
+  dbx4.firebird.base in '..\source\dbx4.firebird.base.pas';
+
+{$LIBPREFIX 'dbx'}
+{$LIBSUFFIX '0'}
+
+begin
+  ReportMemoryLeaksOnShutdown := DebugHook <> 0;
+end.
---
+++ project/fb4.rc.h
@@ -0,0 +1,8 @@
+#define _Compiler
+#define _ProductName "E Stream Software DBExpress 4 FIREBIRD Driver"
+#define _ProductURL "www.sql.com.my/product"
+#define _CompanyName "E Stream Software Sdn Bhd"
+#define _CompanyURL "www.sql.com.my"
+#define _SupportURL "www.sql.com.my/support"
+#define _FileVersion 4,0,0,1
+#define _FileVersionStr "4,0,0,1"
