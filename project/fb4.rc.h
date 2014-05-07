3bba34d0d726a81f4e8af79751cafe5480fb355e
Chau Chee Yang <ccy@sql.com.my>
2014-05-07 08:57:40 +0800

    Remove project, project.d12, project.d14, project.d15, project.d16, project.d17 folders

--- project/fb4.rc.h
+++
@@ -1,8 +0,0 @@
-#define _Compiler
-#define _ProductName "E Stream Software DBExpress 4 FIREBIRD Driver"
-#define _ProductURL "www.sql.com.my/product"
-#define _CompanyName "E Stream Software Sdn Bhd"
-#define _CompanyURL "www.sql.com.my"
-#define _SupportURL "www.sql.com.my/support"
-#define _FileVersion 4,0,0,7
-#define _FileVersionStr "4,0,0,7
