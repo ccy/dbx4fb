87f2c2ca0229e1837ad25eb5c267b122b642dc96
Chau Chee Yang <ccy@sql.com.my>
2022-02-05 13:15:48 +0800

    Remove patch package

--- source/testcase/vcl.dbx.main.pas
+++ source/testcase/vcl.dbx.main.pas
@@ -12,43 +12,28 @@ uses
   vcl.dbx.cmdlines;
 
 function StartApp: Cardinal;
-var P: array of HMODULE;
-    H: HMODULE;
-    i: integer;
 begin
   Result := 0;
 
-  i := 1;
-  {$ifndef Unicode}Inc(i);{$endif}
-  SetLength(P, i);
-
-  P[0] := LoadPackage('SQL.patch.rtl.bpl');
-  {$ifndef Unicode}P[1] := LoadPackage('SQL.patch.dbx.bpl');{$endif}
-
-  try
-    if TCmdLineParams_App.RunAsConsole then begin
-      AllocConsole;
-      try
-        if TCmdLineParams_App.HasTestName then
-          SetConsoleTitle(PChar(TCmdLineParams_App.GetTestName));
-        with TextTestRunner.RunRegisteredTests do begin
-          try
-            if not WasSuccessful then Readln;
-            Result := ErrorCount + FailureCount;
-          finally
-            Free;
-          end;
+  if TCmdLineParams_App.RunAsConsole then begin
+    AllocConsole;
+    try
+      if TCmdLineParams_App.HasTestName then
+        SetConsoleTitle(PChar(TCmdLineParams_App.GetTestName));
+      with TextTestRunner.RunRegisteredTests do begin
+        try
+          if not WasSuccessful then Readln;
+          Result := ErrorCount + FailureCount;
+        finally
+          Free;
         end;
-      finally
-        FreeConsole;
       end;
-    end else begin
-      Application.Initialize;
-      GUITestRunner.RunRegisteredTests;
+    finally
+      FreeConsole;
     end;
-  finally
-    for H in P do
-      UnloadPackage(H);
+  end else begin
+    Application.Initialize;
+    GUITestRunner.RunRegisteredTests;
   end;
 
   TestFramework.ClearRegistry;
