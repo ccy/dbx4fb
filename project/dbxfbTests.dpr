3dff9e1dd5e8a77161ce1e5e7734f9e9b0ca8168
Chau Chee Yang <ccy@sql.com.my>
2023-04-07 13:37:40 +0800

    Remove RSP-39665: StrToSQLTimeStampOffset raise exception: Could not parse SQL TimeStamp string

--- project/dbxfbTests.dpr
+++ project/dbxfbTests.dpr
@@ -2,7 +2,6 @@ program dbxfbTests;
 
 uses
   Winapi.Windows,
-  System.SysUtils.RSP39665 in '..\source\System.SysUtils.RSP39665.pas',
   firebird.utils in '..\source\testcase\firebird.utils.pas',
   vcl.dbx.cmdlines in '..\source\testcase\vcl.dbx.cmdlines.pas',
   vcl.dbx.main in '..\source\testcase\vcl.dbx.main.pas',
